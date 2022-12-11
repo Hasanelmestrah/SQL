48275a27523065d56da38dc259b28055ab04efd6
hasanelmestrah <hasanelmestrah@gmail.com>
2022-12-11 18:41:38 +0200

    Basic Queries

--- sql-exercise-master/answers.sh
+++ sql-exercise-master/answers.sh
@@ -1,1 +1,13 @@
-SELECT * FROM students 
+1/ SELECT Name FROM students;
+
+2/ SELECT * FROM students WHERE Age>30
+
+3/ SELECT Name FROM students WHERE Age=30 AND Gender='F';
+ 
+4/ SELECT Points FROM students WHERE Name='Alex';
+
+5/INSERT INTO students VALUES ('HASAN', 25, 'M', 650);
+
+6/UPDATE students SET Points = Points + 50 WHERE Name='Basma';
+
+7/UPDATE students SET Points = Points - 150 WHERE Name='Alex';
